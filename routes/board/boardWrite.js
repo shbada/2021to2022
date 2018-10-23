eccf1bd116fa92af867569fbaf5daf67263156f0
kimseohae <kimseohae96@bemycar.co.kr>
2018-10-23 10:51:23 +0900

    1. 자유게시판_ 글 작성 개발

--- routes/board/boardWrite.js
+++ routes/board/boardWrite.js
@@ -2,16 +2,29 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: 'localhost',
+var mysqlInfo = {
+    host: '127.0.0.1',
     port: 3306,
-    user: 'daheon',
-    password: 'daheon',
-    database: 'daheon'
-});
+    user: 'seohae',
+    password: 'seohae',
+    database: 'daheonBook'
+};
 
-router.post('',function (req, res, next) {
-    res.send('board Write!')
-})
+var pool = mysql.createPool(mysqlInfo);
+
+router.post('', function(req, res, next){
+    let data = [req.body.title, req.body.content, 1];
+
+    pool.getConnection(function (err, connection) {
+        let sql = "INSERT INTO free_board(title, content, writer_idx) VALUES(?,?,?)";
+
+        connection.query(sql, data, function (err, rows) {
+            if (err) console.error("err : " + err);
+
+            res.send('작성완료되었어요!')
+            connection.release();
+        });
+    });
+});
 
 module.exports = router;
