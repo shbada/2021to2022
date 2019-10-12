94a1cffdca51b7ae95c1bed6d1401b55ba4cbd3f
seohaekim <seohae0406@gmail.com>
2019-10-12 18:13:57 +0900

    add2. redis

---
+++ src/main/java/com/test/technology/configs/TestConfig.java
@@ -0,0 +1,53 @@
+package com.test.technology.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class TestConfig {
+    @Value("${redis.host}")
+    private String host;
+
+    @Value("${redis.port}")
+    private int port;
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory jedisConnectionFactory = null;
+
+        try {
+            RedisStandaloneConfiguration conn = new RedisStandaloneConfiguration();
+            conn.setHostName(host);
+            conn.setPort(port);
+
+            jedisConnectionFactory = new JedisConnectionFactory(conn);
+
+        } catch (Exception e) {
+        }
+
+        return jedisConnectionFactory;
+    }
+
+    @Bean(name="redisTemplate")
+    public RedisTemplate redisTemplate(JedisConnectionFactory jedisConnectionFactory) {
+        final RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericToStringSerializer<>(Object.class));
+        redisTemplate.setValueSerializer(new GenericToStringSerializer<>(Object.class));
+
+        redisTemplate.setConnectionFactory(jedisConnectionFactory);
+//        redisTemplate.setEnableTransactionSupport(true);
+
+        return redisTemplate;
+    }
+
+}
