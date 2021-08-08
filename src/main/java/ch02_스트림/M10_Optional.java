50b55dc6d91805e0a6d246f4fd85cf5f1aaa24e5
seohae <seohae0406@gmail.com>
2021-08-08 11:30:16 +0900

    [08/08] Optional map, flatmap

--- src/main/java/ch02_스트림/M10_Optional.java
+++ src/main/java/ch02_스트림/M10_Optional.java
@@ -1,8 +1,9 @@
 package ch02_스트림;
 
+import dto.Product.Job;
+import dto.Product.Person;
 import dto.Product.Product;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public class M10_Optional {
@@ -29,6 +30,28 @@ public class M10_Optional {
         Optional<Product> ofProduct = Optional.ofNullable(product1);
         Optional<String> name = ofProduct.map(Product::getProductName); // name 에 값을 넣는다. Optional 이 비어져있으면 아무일도 일어나지 않는다.
 
+        // 여러 get 메소드 연결
+        Product product5 = new Product(0, "Note_red", 1, 100);
+        Person person = new Person(1, "seohae");
+        Job job = new Job(1, "dev");
+
+        Optional<Job> ofJob = Optional.ofNullable(job);
+        person.setJob(ofJob);
+
+        Optional<Person> ofPerson = Optional.ofNullable(person);
+        product5.setPerson(ofPerson);
+
+        Optional<Product> optProduct = Optional.of(product5);
+
+//        Optional<String> getPersonName = optProduct.map(Product::getPerson) // Optional<Optional<Person>>
+//                                                   .map(Person::getJob)
+//                                                   .map(Job::getJobName);
+
+        Optional<String> getPersonName = optProduct.flatMap(Product::getPerson)
+                                                   .flatMap(Person::getJob)
+                                                   .map(Job::getJobName);
+
+        System.out.println(getPersonName);
 
     }
 }
--- src/main/java/dto/Product/Product.java
+++ src/main/java/dto/Product/Product.java
@@ -3,11 +3,15 @@ package dto.Product;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Optional;
+
 public class Product {
     private int idx;
     private String productName;
     private int ordCnt;
     private int totalCnt;
+
+    private Optional<Person> person;
 
     public int getIdx() {
         return idx;
@@ -23,6 +27,14 @@ public class Product {
 
     public int getTotalCnt() {
         return totalCnt;
+    }
+
+    public Optional<Person> getPerson() {
+        return person;
+    }
+
+    public void setPerson(Optional<Person> person) {
+        this.person = person;
     }
 
     public Product(int idx, String productName, int ordCnt, int totalCnt) {
