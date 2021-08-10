88dc4a7340e64769592b0b3d10de05ef90e15460
seohae <seohae0406@gmail.com>
2021-08-10 18:48:11 +0900

    [08/10] 공정위 API 호출 배치 job

--- src/main/java/com/study/batch/jobs/M02_vendor/VendorItemReaderJobConfig.java
+++ src/main/java/com/study/batch/jobs/M02_vendor/VendorItemReaderJobConfig.java
@@ -1,10 +1,16 @@
 package com.study.batch.jobs.M02_vendor;
 
+import com.study.batch.jobs.M01_csvFile.entity.TempLibrary;
+import com.study.batch.jobs.M01_csvFile.entity.dto.TempLibraryDto;
+import com.study.batch.jobs.M01_csvFile.steps.CsvFileItemTskletStep;
+import com.study.batch.jobs.M02_vendor.steps.VendorItemReaderStep;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,6 +24,7 @@ public class VendorItemReaderJobConfig {
     private static final String JOB_NAME = "vendorItemReaderJob";
     private static final int chunkSize = 100;
 
+    private final VendorItemReaderStep vendorItemReaderStep;
 
     /**
      * Job
@@ -25,11 +32,36 @@ public class VendorItemReaderJobConfig {
      */
     @Bean(name = JOB_NAME)
     public Job vendorItemReaderJob() {
-//        return jobBuilderFactory.get("vendorItemReaderJob")
-//                .incrementer(new RunIdIncrementer())
-//                .start(test())
-//                .next(test())
-//                .build();
-        return null;
+        return jobBuilderFactory.get("vendorItemReaderJob")
+                .incrementer(new RunIdIncrementer())
+                .start(vendorItemTasklsetStep())
+                .next(vendorItemReaderStep())
+                .build();
+    }
+
+    /**
+     * 공정위 데이토포털 API 호출
+     * @return
+     */
+    @Bean(name = JOB_NAME +"_task_step")
+    public Step vendorItemTasklsetStep(){
+        return stepBuilderFactory.get("csvFileItemTasklsetStep")
+                /* 엑셀 파일 읽고, static 변수에 저장 */
+                .tasklet(new CsvFileItemTskletStep())
+                .build();
+    }
+
+    /**
+     * API Response
+     * @return
+     */
+    @Bean(name = JOB_NAME +"_step")
+    public Step vendorItemReaderStep() {
+        return stepBuilderFactory.get("vendorItemReaderStep")
+                .<String, String>chunk(chunkSize)
+                .reader(vendorItemReaderStep.reader())
+                //.processor(vendorItemReaderStep.processor())
+                .writer(vendorItemReaderStep.writer() )
+                .build();
     }
 }
