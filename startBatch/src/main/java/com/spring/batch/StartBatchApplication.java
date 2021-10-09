a68a006ed5bea27f5a62dc726b24f229a47a3e8b
seohae <seohae0406@gmail.com>
2021-10-09 17:25:42 +0900

    기본적인 잡 실행 (helloJob)

--- startBatch/src/main/java/com/spring/batch/StartBatchApplication.java
+++ startBatch/src/main/java/com/spring/batch/StartBatchApplication.java
@@ -1,9 +1,11 @@
 package com.spring.batch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing /* 배치 프로세싱을 하겠다는 의미로 선언 */
 public class StartBatchApplication {
 
     public static void main(String[] args) {
