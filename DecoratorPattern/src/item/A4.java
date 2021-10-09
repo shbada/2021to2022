9e36607a5032849d79abae93fc452fee6f65ce44
seohae <seohae0406@gmail.com>
2021-10-10 00:03:34 +0900

    (10.10 공부) 데코레이터 패턴

---
+++ DecoratorPattern/src/item/A4.java
@@ -0,0 +1,9 @@
+package item;
+
+import item.decorator.AudiDecorator;
+
+public class A4 extends AudiDecorator {
+    public A4(ICar audi, String modelName) {
+        super(audi, modelName, 2000);
+    }
+}
