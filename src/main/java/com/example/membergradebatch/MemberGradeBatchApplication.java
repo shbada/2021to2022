d163d8716ec399e503b5f787107b8f231a36d4bc
seohae <seohae0406@gmail.com>
2021-10-13 12:25:18 +0900

    (10.13 공부) project init

---
+++ src/main/java/com/example/membergradebatch/MemberGradeBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.membergradebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberGradeBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberGradeBatchApplication.class, args);
+    }
+
+}
