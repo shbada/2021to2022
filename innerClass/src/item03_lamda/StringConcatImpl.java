5523da9575ec15df4a30c3e71813b7073baf21f4
seohae <seohae0406@gmail.com>
2021-10-14 22:37:34 +0900

    (10.14 공부) lamda

---
+++ innerClass/src/item03_lamda/StringConcatImpl.java
@@ -0,0 +1,8 @@
+package item03_lamda;
+
+public class StringConcatImpl implements StringConcat {
+    @Override
+    public void makeString(String s1, String s2) {
+        System.out.println(s1 + "," + s2);
+    }
+}
