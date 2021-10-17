f7e544971660b36b422513acbd1ff98ffb742545
seohae <seohae0406@gmail.com>
2021-10-17 16:16:22 +0900

    (10.17 공부) item17

---
+++ src/main/java/item17/ComplexStaticField.java
@@ -0,0 +1,7 @@
+package item17;
+
+public class ComplexStaticField {
+    public static final Complex ZERO = new Complex(0,0);
+    public static final Complex ONE = new Complex(1,0);
+    public static final Complex I = new Complex(0,1);
+}
