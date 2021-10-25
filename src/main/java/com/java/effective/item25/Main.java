b20344c70cfc753c5c77797b7c6f2b075f4f7d9e
seohae <seohae0406@gmail.com>
2021-10-25 13:01:59 +0900

    (10.25 공부) item25

---
+++ src/main/java/com/java/effective/item25/Main.java
@@ -0,0 +1,8 @@
+package com.java.effective.item25;
+
+public class Main {
+    public static void main(String[] args) {
+        // pancake
+        System.out.println(Utensil.NAME + Dessert.NAME);
+    }
+}
