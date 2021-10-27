6f0788803d8bfa08387b1e62f22b02daeea70ead
seohae <seohae0406@gmail.com>
2021-10-27 22:21:00 +0900

    (10.27 공부) item27

---
+++ src/main/java/com/java/effective/item27/Main.java
@@ -0,0 +1,13 @@
+package com.java.effective.item27;
+
+import com.java.effective.item20.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        // Set<Test> test = new HashSet();
+        Set<Test> test = new HashSet<>();
+    }
+}
---
+++ src/main/java/com/java/effective/item27/Main2.java
@@ -0,0 +1,14 @@
+package com.java.effective.item27;
+
+import com.java.effective.item20.Test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main2 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+    }
+}
