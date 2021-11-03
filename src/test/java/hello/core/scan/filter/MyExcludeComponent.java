419c05bac338f8effbb9d5531f6d9d17ead28469
seohae <seohae0406@gmail.com>
2021-11-03 23:09:49 +0900

    (11.03 공부) @ComponentScan filter

---
+++ src/test/java/hello/core/scan/filter/MyExcludeComponent.java
@@ -0,0 +1,9 @@
+package hello.core.scan.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE) // TYPE 또는 필드 (TYPE : 클래스레벨에 붙는것 )
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyExcludeComponent {
+}
