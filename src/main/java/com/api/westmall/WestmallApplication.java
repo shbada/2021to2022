788cd9591f9070140d83d7673cc980c8d67ea839
seohae <seohae0406@gmail.com>
2021-11-18 21:11:13 +0900

    (11.18) project init

---
+++ src/main/java/com/api/westmall/WestmallApplication.java
@@ -0,0 +1,13 @@
+package com.api.westmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WestmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WestmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/westmall/WestmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.westmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WestmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
