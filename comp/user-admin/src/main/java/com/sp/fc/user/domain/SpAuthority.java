0050ee0ce7fe3ca865a8d79c4e8d855df1f04460
seohae <seohae0406@gmail.com>
2021-11-23 17:13:08 +0900

    (11.23) login user add (h2 db)

---
+++ comp/user-admin/src/main/java/com/sp/fc/user/domain/SpAuthority.java
@@ -0,0 +1,32 @@
+package com.sp.fc.user.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * 유저 권한 테이블
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="sp_user_authority")
+@IdClass(SpAuthority.class)
+public class SpAuthority implements GrantedAuthority {
+
+    @Id
+    @Column(name="user_id")
+    private Long userId;
+
+    @Id
+    private String authority;
+
+
+}
--- server/login-userdetails/src/main/java/com/sp/fc/web/UserDetailsTestApplication.java
+++ server/login-userdetails/src/main/java/com/sp/fc/web/UserDetailsTestApplication.java
@@ -2,6 +2,8 @@ package com.sp.fc.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * UsernamePasswordAuthenticationFilter 는 UsernamePasswordAuthenticationToken을 가지고 ProviderManager 로 넘겨주고,
@@ -10,7 +12,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * UserDetails 라는 Principal 객체를 받도록 되어있고,
  * 이 객체를 가지고 UsernamePasswordAuthenticationToken 토큰에 인증된 사용자라면 넣어서 리턴을 하도록 구조화되어있다.
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.sp.fc.user",
+        "com.sp.fc.web"
+})
+@EntityScan(basePackages = {
+        "com.sp.fc.user.domain"
+})
+@EnableJpaRepositories(basePackages = {
+        "com.sp.fc.user.repository"
+})
 public class UserDetailsTestApplication {
 
     public static void main(String[] args) {
