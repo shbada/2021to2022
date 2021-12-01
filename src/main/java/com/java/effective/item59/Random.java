af99e8afb9a651673134d29a426320b49a9fb27e
seohae <seohae0406@gmail.com>
2021-12-01 12:10:13 +0900

    (12.01 공부) item59

---
+++ src/main/java/com/java/effective/item59/Random.java
@@ -0,0 +1,22 @@
+package com.java.effective.item59;
+
+public class Random {
+    static java.util.Random rnd = new java.util.Random();
+
+    static int random(int n) {
+        return Math.abs(rnd.nextInt()) % n;
+    }
+
+    public static void main(String[] args) {
+        int n = 2 * (Integer.MAX_VALUE / 3);
+        int low = 0;
+
+        for (int i = 0; i < 1000000; i++) {
+            if (random(n) < n / 2) {
+                low++;
+            }
+        }
+
+        System.out.println(low);
+    }
+}
