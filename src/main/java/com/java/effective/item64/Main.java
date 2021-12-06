49d1fc2012a2fa396d38aa613dde05f07cbe97da
seohae <seohae0406@gmail.com>
2021-12-06 21:10:04 +0900

    (12.06 공부) item64

---
+++ src/main/java/com/java/effective/item64/Main.java
@@ -0,0 +1,10 @@
+package com.java.effective.item64;
+
+import java.util.LinkedHashSet;
+
+public class Main {
+    public static void main(String[] args) {
+//        Set<Person> test = new LinkedHashSet<Person>();
+//        LinkedHashSet<Person> test = new LinkedHashSet<Person>();
+    }
+}
