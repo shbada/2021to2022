2276a80b63a692a51b5c00c9c47e7fcb05c4631e
seohae <seohae0406@gmail.com>
2021-12-12 15:56:36 +0900

    (12.12) HTTP 요청 파라미터 - @RequestParam

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -31,5 +31,98 @@ public class RequestParamController {
         log.info("username={}, age={}", username, age);
         response.getWriter().write("ok");
     }
+
+    /**
+     * @RequestParam 사용
+     * - 파라미터 이름으로 바인딩
+     *
+     * @ResponseBody 추가
+     * - View 조회를 무시하고, HTTP message body에 직접 해당 내용 입력
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(@RequestParam("username") String memberName,
+                                 @RequestParam("age") int memberAge) {
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
+    }
+
+    /**
+     * @RequestParam 사용
+     * HTTP 파라미터 이름이 변수 이름과 같으면 @RequestParam(name="xx") 생략 가능
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(@RequestParam String username,
+                                 @RequestParam int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * @RequestParam 사용
+     * String, int 등의 단순 타입이면 @RequestParam 도 생략 가능
+     *
+     * @RequestParam 애노테이션을 생략하면 스프링 MVC는 내부에서 required=false 를 적용
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * @RequestParam.required
+     * /request-param -> username이 없으므로 예외 *
+     *
+     * 주의!
+     * 필수여도 빈문자열이면 통과됨 (null일 경우는 400 에러가 발생)
+     * /request-param?username= -> 빈문자로 통과 *
+     *
+     * 주의!
+     * /request-param
+     * age 는 required=false 여도 아예 key 값도 존재하지 않다면? 500 에러가 발생한다.
+     * int 에는 null을 넣을 수가 없다.
+     * int age -> null을 int에 입력하는 것은 불가능, 따라서 Integer 변경해야 함(또는 다음에 나오는 defaultValue 사용)
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(@RequestParam(required = true) String username,
+                                       @RequestParam(required = false) Integer age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * @RequestParam
+     * - defaultValue 사용 *
+     * 참고: defaultValue는 빈 문자의 경우에도 적용 * /request-param?username=
+     *
+     *
+     * @RequestParam(required = false, defaultValue = "-1")
+     * 여기선 required 가 의미가 없다. defaultValue가 셋팅되니깐.
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(@RequestParam(required = true, defaultValue = "guest") String username,
+                                      @RequestParam(defaultValue = "-1") int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * @RequestParam Map, MultiValueMap
+     * Map(key=value)
+     * MultiValueMap(key=[value1, value2, ...] ex) (key=userIds, value=[id1, id2])
+     *
+     * 파라미터의 값이 1개가 확실하다면 Map 을 사용해도 되지만, 그렇지 않다면 MultiValueMap 을 사용하자.
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
 }
 
