6bf270f665dcca68584b396dc5a187c815234789
seohae <seohae0406@gmail.com>
2021-12-12 22:32:36 +0900

    (12.12) jwt test

---
+++ src/main/java/com/test/jwt/JwtLibraryTestApplication.java
@@ -0,0 +1,13 @@
+package com.test.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtLibraryTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtLibraryTestApplication.class, args);
+    }
+
+}
