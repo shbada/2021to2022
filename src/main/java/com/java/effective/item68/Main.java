b14ecb48c09c0a15b1c45c46114ee9b4ef012057
seohae <seohae0406@gmail.com>
2021-12-12 11:06:20 +0900

    (12.12 공부) item69

---
+++ src/main/java/com/java/effective/item68/Main.java
@@ -0,0 +1,22 @@
+package com.java.effective.item68;
+
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+//            int i = 0;
+//
+//            while(true) {
+//                range[i++].climb();
+//            }
+
+//            for (Mountain m : range) {
+//                m.climb();
+//            }
+            //Iterator
+        } catch (ArrayIndexOutOfBoundsException e) {
+
+        }
+    }
+}
