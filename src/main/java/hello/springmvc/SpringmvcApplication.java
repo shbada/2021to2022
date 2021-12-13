2bcb9c555a30fa411aa336e668b9c5f45c896cc8
seohae <seohae0406@gmail.com>
2021-12-13 16:37:02 +0900

    (12.13) HTTP response 3 case

--- src/main/java/hello/springmvc/SpringmvcApplication.java
+++ src/main/java/hello/springmvc/SpringmvcApplication.java
@@ -10,4 +10,21 @@ public class SpringmvcApplication {
         SpringApplication.run(SpringmvcApplication.class, args);
     }
 
+    /**
+     * response
+     *
+     * - 정적 리소스
+     * 예) 웹 브라우저에 정적인 HTML, css, js을 제공할 때는, 정적 리소스를 사용한다.
+     * 다음 디렉토리에 리소스를 넣어두면 스프링 부트가 정적 리소스로 서비스를 제공한다
+     * /static , /public , /resources , /META-INF/resources
+     *
+     * - 뷰 템플릿 사용
+     * 예) 웹 브라우저에 동적인 HTML을 제공할 때는 뷰 템플릿을 사용한다.
+     * src/main/resources/templates
+     * src/main/resources/templates/response/hello.html
+     *
+     * - HTTP 메시지 사용
+     * HTTP API를 제공하는 경우에는 HTML이 아니라 데이터를 전달해야 하므로,
+     * HTTP 메시지 바디에 JSON 같은 형식으로 데이터를 실어 보낸다.
+     */
 }
