e147ea1c1e79d27efda13a1dd1c7083bdce6902d
seohae <seohae0406@gmail.com>
2021-12-15 21:12:07 +0900

    (12.15) item 73

---
+++ src/main/java/com/java/effective/item73/Main.java
@@ -0,0 +1,15 @@
+package com.java.effective.item73;
+
+import java.util.AbstractSequentialList;
+
+public class Main {
+    public static void main(String[] args) {
+//        try {
+//            // ... // 저수준 추상화를 이용한다.
+//        } catch (LowerLevelException e) {
+//            throw new HigherLevelException();
+//        }
+
+        // HigherLevelException
+    }
+}
