76b8dc83e195390dca8c3f52a424a2ad6870ae5b
seohae <seohae0406@gmail.com>
2021-12-20 17:06:41 +0900

    (12.20) item 76

---
+++ src/main/java/com/java/effective/item76/Main.java
@@ -0,0 +1,12 @@
+package com.java.effective.item76;
+
+import com.java.effective.item29.Stack;
+
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) {
+        // Stack
+        // TreeMap
+    }
+}
