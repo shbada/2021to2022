0c97a700ab5c975d9f725ec007bfe1c6546b81b5
seohae <seohae0406@gmail.com>
2022-01-10 12:05:08 +0900

    (01/10) 템플릿 메서드 패턴 적용

---
+++ src/main/java/hello/advanced/app/v4/OrderControllerV4.java
@@ -0,0 +1,45 @@
+package hello.advanced.app.v4;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 로그 추적기
+ * 애플리케이션이 커지면서 모니터링/운영이 중요해지는 단계다.
+ * 어떤 부분에서 병목이 발생하고, 예외가 발생하는지 로그를 통해 확인을 해야한다.
+ * 로그를 미리 남겨둔다면 이런 부분을 손쉽게 찾을 수 있다.
+ *
+ * 로그를 남긴다고 해서, 비즈니스 로직의 동작에 영향을 주면 안된다.
+ * 특정 ID를 남겨서 어떤 HTTP 요청에서 시작된것인지 명확하게 구분이 가능해야한다.
+ * 트랜잭션 ID, 여기서는 하나의 HTTP 요청이 시작해서 끝날때 까지를 하나의 트랜잭션이라 한다.
+ */
+@RestController // (@Controller + @ResponseBody)
+@RequiredArgsConstructor
+public class OrderControllerV4 {
+
+    private final OrderServiceV4 orderService;
+    private final LogTrace trace;
+
+    /**
+     * localhost:8080/v4/request?itemId=hello
+     * 같은 HTTP 요청에 대해서 트랜잭션ID 가 유지, level 도 정상 노출됨
+     * @param itemId
+     * @return
+     */
+    @GetMapping("/v4/request")
+    public String request(String itemId) {
+        AbstractTemplate<String> template = new AbstractTemplate<>(trace) {
+            @Override
+            protected String call() {
+                orderService.orderItem(itemId);
+                return "ok";
+            }
+        };
+
+        return template.execute("OrderController.request()");
+    }
+}
--- src/test/java/hello/advanced/template/code/SubClassLogic1.java
+++ src/test/java/hello/advanced/template/code/SubClassLogic1.java
@@ -2,6 +2,11 @@ package hello.advanced.template.code;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * 자식클래스는 부모클래스의 로직을 사용하지 않는다.
+ * 하지만 상속해서 받고있따.
+ * 자식클래스가 부모클래스를 의존하게되고, 부모클래스에 수정이 발생하면 자식클래스도 변경이 필요하다.
+ */
 @Slf4j
 public class SubClassLogic1 extends AbstractTemplate {
     /**
