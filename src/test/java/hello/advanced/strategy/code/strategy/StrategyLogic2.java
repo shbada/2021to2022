52803b0b66d67a939aaf87ab0e97f4157cc34472
seohae <seohae0406@gmail.com>
2022-01-11 16:58:41 +0900

    (01/11) 전략 패턴 적용

---
+++ src/test/java/hello/advanced/strategy/code/strategy/StrategyLogic2.java
@@ -0,0 +1,12 @@
+package hello.advanced.strategy.code.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class StrategyLogic2 implements Strategy {
+
+    @Override
+    public void call() {
+        log.info("비즈니스 로직2 실행");
+    }
+}
