bd01636541334b78dfe1f4dee405fb2625d6ad81
seohae <seohae0406@gmail.com>
2022-01-20 12:12:32 +0900

    (01.20) project package

---
+++ src/test/java/com/redo/studyolle/modules/controller/AccountControllerTest.java
@@ -0,0 +1,10 @@
+package com.redo.studyolle.modules.controller;
+
+import com.redo.studyolle.infra.MockMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MockMvcTest
+class AccountControllerTest {
+
+}
