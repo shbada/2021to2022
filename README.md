8a31d0cc21873a192f2b44d77626e9b47a0da867
seohae <seohae0406@gmail.com>
2022-01-20 11:48:10 +0900

    (01.20) project init

---
+++ README.md
@@ -0,0 +1,14 @@
+StudyOlle_rode
+- 강의로 배운 내용을 바탕으로 혼자 직접 구현해보기
+
+#### 환경구성
+01) SpringBoot  
+02) Java11  
+03) Maven  
+04) JPA  
+05) SpringSecurity  
+06) Junit Test  
+07) Mysql  
+08) Postgresql  
+09) ModelMapper  
+10) thymeleaf  
---
+++ src/test/java/com/redo/studyolle/StudyolleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redo.studyolle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyolleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
