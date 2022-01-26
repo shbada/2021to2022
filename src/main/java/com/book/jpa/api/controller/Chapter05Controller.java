9d9d2fc20c9b15486685bc4a3cb4aeefe0287493
seohae <seohae0406@gmail.com>
2022-01-26 16:22:54 +0900

    (01/26) 연관관계 주인

--- src/main/java/com/book/jpa/api/controller/Chapter05Controller.java
+++ src/main/java/com/book/jpa/api/controller/Chapter05Controller.java
@@ -87,4 +87,40 @@ public class Chapter05Controller {
 
         return "ok";
     }
+
+    /**
+     * 양방향 연관관계 저장
+     * @return
+     */
+    @PutMapping("/team/save")
+    public String saveMemberTeam() {
+        // 팀1 저장
+        Team team1 = new Team();
+        team1.setName("팀1");
+        teamRepository.save(team1);
+
+        // 회원1 저장
+        Member member1 = new Member();
+        member1.setUsername("회원1");
+        /**
+         * 연관관계 주인은 Member 다.
+         * setTeam()을 하지 않으면 Member 의 team_ID는 null이다.
+         * 연관관계의 주인만이 외래 키의 값을 변경할 수 있다.
+         */
+        member1.setTeam(team1); // 연관관계 설정
+        memberRepository.save(member1);
+
+        Member member2 = new Member();
+        member2.setUsername("회원2");
+        member2.setTeam(team1); // 연관관계 설정
+        memberRepository.save(member2);
+
+        /* 무시) 연관관계의 주인이 team 이 아니므로 의미없다. */
+        // 그래도 넣어줘야한다.
+        // 테스트에서 team1.getMembers(); 를 출력했을때 아래 코드가 없으면 0 인데, 기대한 값은 2일 것이다.
+        team1.getMembers().add(member1);
+        team1.getMembers().add(member2);
+
+        return "ok";
+    }
 }
--- src/main/java/com/book/jpa/chapter05/Team.java
+++ src/main/java/com/book/jpa/chapter05/Team.java
@@ -28,4 +28,16 @@ public class Team {
      */
     // @OneToMany
     // private List<Member> members;
+
+    /**
+     * 양방향 연관관계 매핑시
+     * 두 연관관계 중 하나를 연관관계 주인으로 정해야한다.
+     * 연관관계의 주인만이 데이터베이스 연관관계와 매핑되고 외래키를 관리(등록/수정/삭제) 할 수 잇다.
+     * 반면에 주인이 아닌쪽(team)은 읽기만 할 수 있다.
+     *
+     * 1) 주인(Member)는 mappedBy 속성을 사용하지 않는다.
+     * 2) 주인이 아니면(Team) mappedBy 속성을 사용해서 값으로 연관관계의 주인을 지정해야한다.
+     */
+     @OneToMany(mappedBy = "team") // team 은 주인이 아님을 설장한다. 주인은 Member.team 이다.
+     private List<Member> members;
 }
