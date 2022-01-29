69badad3c3315e80624d65a1fc787f37977c398a
seohae <seohae0406@gmail.com>
2022-01-29 18:11:31 +0900

    project init

---
+++ src/test/java/com/api/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
