baf8c128833972b675ea5d02126686bd930abf5c
seohae <seohae0406@gmail.com>
2022-02-04 20:41:59 +0900

    (02.04) 프록시 팩토리 구현

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,11 +1,7 @@
 package hello.proxy;
 
-import hello.proxy.config.AppV1Config;
-import hello.proxy.config.AppV2Config;
-import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
-import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -22,7 +18,9 @@ import org.springframework.context.annotation.Import;
 //@Import({InterfaceProxyConfig.class}) // 생성한 파일 import (proxy 적용)
 //@Import({ConcreteProxyConfig.class}) // 생성한 파일 import (proxy 적용)
 //@Import({DynamicProxyBasicConfig .class}) // 생성한 파일 import (proxy 적용)
-@Import({DynamicProxyFilterConfig.class}) // 생성한 파일 import (proxy 적용)
+//@Import({DynamicProxyFilterConfig.class}) // 생성한 파일 import (proxy 적용)
+//@Import({ProxyFactoryConfigV1.class}) // 생성한 파일 import (proxy 적용)
+@Import({ProxyFactoryConfigV2.class}) // 생성한 파일 import (proxy 적용)
 /**
  * 현재 내가 존재하는 패키지와 그 하위를 모두 ComponentScan 하는데,
  * hello.proxy.app 을 하면 이 경로의 패키지만 ComponentScan 한다.
