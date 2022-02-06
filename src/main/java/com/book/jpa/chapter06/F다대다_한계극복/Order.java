101f241fe92d79d7042eb9cf65b58303ebcb56e4
seohae <seohae0406@gmail.com>
2022-02-06 21:40:55 +0900

    (02/06) 다대다매핑 완료

---
+++ src/main/java/com/book/jpa/chapter06/F다대다_한계극복/Order.java
@@ -0,0 +1,30 @@
+package com.book.jpa.chapter06.F다대다_한계극복;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+/**
+ * MemberProduct 를 Order 엔티티로 새로 생성하는게 편할 수도 있다.
+ */
+@Entity
+@Setter
+@Getter
+public class Order {
+    @Id
+    @Column(name= "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private MemberF memberF;
+
+    @ManyToOne
+    @JoinColumn(name = "PRODUCT_ID")
+    private ProductF productF;
+
+    /* 컬럼 추가 */
+    private int orderAmount;
+}
