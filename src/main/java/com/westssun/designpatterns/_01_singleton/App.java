6c544268ba7ae3ab7db8271a5a4577a2c2d7046f
seohae <seohae0406@gmail.com>
2022-02-07 22:27:58 +0900

    (02/07) 싱글톤 패턴 3부 - 싱글톤 패턴 구현 방법을 깨트리는 방법

--- src/main/java/com/westssun/designpatterns/_01_singleton/App.java
+++ src/main/java/com/westssun/designpatterns/_01_singleton/App.java
@@ -1,7 +1,10 @@
 package com.westssun.designpatterns._01_singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         // Settings1 settings1 = new Settings1();
         // Settings1 settings2 = new Settings1();
 
@@ -19,5 +22,17 @@ public class App {
 
         // false (싱글톤 패톤 적용 후)
         System.out.println(settings1 != settings2);
+        // 얘를 어떻게 true 로 할까?
+
+        /** 싱글톤을 깨뜨리는 방법 */
+        /* 1) 리플렉션 사용 */
+        Constructor<Settings1> declaredConstructor = Settings1.class.getDeclaredConstructor();
+        declaredConstructor.setAccessible(true); // private 접근 가능
+        Settings1 settings3 = declaredConstructor.newInstance();
+
+        System.out.println(settings1 != settings3);
+
+        /* 2) 직렬화, 역직렬화 사용 */
+
     }
 }
---
+++ src/main/java/com/westssun/designpatterns/_01_singleton/AppSerialize.java
@@ -0,0 +1,32 @@
+package com.westssun.designpatterns._01_singleton;
+
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class AppSerialize {
+    public static void main(String[] args) {
+        /** 싱글톤을 깨뜨리는 방법 */
+        /* 2) 직렬화, 역직렬화 사용 */
+        Settings1Serialize settings1 = Settings1Serialize.getInstance();
+
+        // 객체를 직렬화하여 파일로 작성
+        try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings1.obj"))) {
+            out.writeObject(settings1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 역직렬화
+        Settings1Serialize settings2 = null;
+        try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings1.obj"))) {
+            settings2 = (Settings1Serialize) in.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // false
+        // Settings1Serialize 에 readResolve() 구현시 true 가 나온다.
+        System.out.println(settings1 == settings2);
+    }
+}
