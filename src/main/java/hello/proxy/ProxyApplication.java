3b4855c460b20c3bcc19a1db365e15ab96649331
seohae <seohae0406@gmail.com>
2022-02-08 12:37:58 +0900

    (02.08) Aspect annotation

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -4,6 +4,7 @@ import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
 import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
+import hello.proxy.config.v6_aop.AopConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -24,7 +25,8 @@ import org.springframework.context.annotation.Import;
 //@Import({ProxyFactoryConfigV1.class}) // 생성한 파일 import (proxy 적용)
 //@Import({ProxyFactoryConfigV2.class}) // 생성한 파일 import (proxy 적용)
 //@Import({BeanPostProcessorConfig.class}) // 생성한 파일 import (proxy 적용)
-@Import({AutoProxyConfig.class}) // 생성한 파일 import (proxy 적용)
+//@Import({AutoProxyConfig.class}) // 생성한 파일 import (proxy 적용)
+@Import({AopConfig.class}) // 생성한 파일 import (proxy 적용)
 /**
  * 현재 내가 존재하는 패키지와 그 하위를 모두 ComponentScan 하는데,
  * hello.proxy.app 을 하면 이 경로의 패키지만 ComponentScan 한다.
