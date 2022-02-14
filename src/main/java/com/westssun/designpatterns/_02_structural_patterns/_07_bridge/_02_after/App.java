82d0be586b3569b5c260842041809e0749e936f4
seohae <seohae0406@gmail.com>
2022-02-14 11:06:07 +0900

    (02/14) 브릿지 패턴

---
+++ src/main/java/com/westssun/designpatterns/_02_structural_patterns/_07_bridge/_02_after/App.java
@@ -0,0 +1,27 @@
+package com.westssun.designpatterns._02_structural_patterns._07_bridge._02_after;
+
+import com.westssun.designpatterns._02_structural_patterns._07_bridge._01_before.Champion;
+
+/**
+ * 브릿지 패턴
+ * 추상적인것과 구체적인것을 분리하여 연결하는 패턴
+ *
+ * [장점]
+ * 추상적인 코드를 구체적인 코드 변경 없이도 독립적으로 확장할 수 있다.
+ * 추상적인 코드와 구체적인 코드를 분리할 수 있다.
+ *
+ * [단점]
+ * 계층 구조가 늘어나 복잡도가 증가할 수 있다.
+ */
+public abstract class App implements Champion {
+
+    public static void main(String[] args) {
+        Champion kda아리 = new 아리(new KDA());
+        kda아리.skillQ();
+        kda아리.skillW();
+
+        Champion poolParty아리 = new 아리(new PoolParty());
+        poolParty아리.skillR();
+        poolParty아리.skillW();
+    }
+}
---
+++ src/main/java/com/westssun/designpatterns/_02_structural_patterns/_07_bridge/package-info.java
@@ -0,0 +1,1 @@
+package com.westssun.designpatterns._02_structural_patterns._07_bridge;
