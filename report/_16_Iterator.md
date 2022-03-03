acde164f709a8ebbeba0288d84d23b80384661fe
seohae <seohae0406@gmail.com>
2022-03-03 21:09:04 +0900

    (03.03) 이터레이터 패턴 (복습)

---
+++ report/_16_Iterator.md
@@ -0,0 +1,6 @@
+#### 이터레이터 (Iterator) 패턴
+- 집합 객체 내부 구조를 노출시키지 않고 순회 하는 방법을 제공하는 패턴.
+![IMAGES](../report/images/iterator01.png)     
+
+(1) 적용 후
+![IMAGES](../report/images/iterator02.png)
---
+++ src/main/java/com/designpattern/report/_16_iterator/step1_before/Client.java
@@ -0,0 +1,41 @@
+package com.designpattern.report._16_iterator.step1_before;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 집합 객체를 순회
+ * 내부 구현을 드러내지 않고 클라이언트가 내부 안에 들어있는 집합 객체를 순회하는 방법
+ *
+ * 기본적으로 사용한 Iterator.java 인터페이스가 대표적
+ *
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        Board board = new Board();
+        board.addPost("디자인 패턴 게임");
+        board.addPost("선생님, 저랑 디자인 패턴 하나 학습하시겠습니까?");
+        board.addPost("지금 이 자리에 계신 여러분들은 모두 디자인 패턴을 학습하고 계신 분들입니다.");
+
+        /**
+         * board 가 어떤 구조로 되어있는지를 클라이언트가 알아야한다. (List 등)
+         * 만약에 board 가 List -> Set 으로 변경되면?
+         * 클라이언트가 board 변경에 따른 영향을 받는다.
+         */
+        // TODO 들어간 순서대로 순회하기
+        List<Post> posts = board.getPosts();
+        for (int i = 0 ; i < posts.size() ; i++) {
+            Post post = posts.get(i);
+            System.out.println(post.getTitle());
+        }
+
+        // TODO 가장 최신 글 먼저 순회하기
+        Collections.sort(posts, (p1, p2) -> p2.getCreatedDateTime().compareTo(p1.getCreatedDateTime()));
+        for (int i = 0 ; i < posts.size() ; i++) {
+            Post post = posts.get(i);
+            System.out.println(post.getTitle());
+        }
+    }
+
+}
