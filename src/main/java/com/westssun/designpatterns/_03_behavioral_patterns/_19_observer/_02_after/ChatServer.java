cb0a9e0129e8bb964947147b6710cb3e6a54d011
seohae <seohae0406@gmail.com>
2022-03-03 23:30:28 +0900

    (03.03) 옵저버 패턴

---
+++ src/main/java/com/westssun/designpatterns/_03_behavioral_patterns/_19_observer/_02_after/ChatServer.java
@@ -0,0 +1,54 @@
+package com.westssun.designpatterns._03_behavioral_patterns._19_observer._02_after;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Subject
+ */
+public class ChatServer {
+
+    private Map<String, List<Subscriber>> subscribers = new HashMap<>();
+
+    /**
+     * subscribe
+     * @param subject
+     * @param subscriber
+     */
+    public void register(String subject, Subscriber subscriber) {
+        if (this.subscribers.containsKey(subject)) {
+            this.subscribers.get(subject).add(subscriber);
+        } else {
+            List<Subscriber> list = new ArrayList<>();
+            list.add(subscriber);
+            this.subscribers.put(subject, list); // List.of()는 immutable 이므로 원소 추가가 불가능하다.
+        }
+    }
+
+    /**
+     * unsubscribe
+     * @param subject
+     * @param subscriber
+     */
+    public void unregister(String subject, Subscriber subscriber) {
+        if (this.subscribers.containsKey(subject)) {
+            this.subscribers.get(subject).remove(subscriber); // 언제 remove 될지 예상 불가능
+        }
+    }
+
+    /**
+     * notify
+     * @param user
+     * @param subject
+     * @param message
+     */
+    public void sendMessage(User user, String subject, String message) {
+        if (this.subscribers.containsKey(subject)) {
+            String userMessage = user.getName() + ": " + message;
+            this.subscribers.get(subject).forEach(s -> s.handleMessage(userMessage));
+        }
+    }
+
+}
---
+++ src/main/java/com/westssun/designpatterns/_03_behavioral_patterns/_19_observer/_02_after/Subscriber.java
@@ -0,0 +1,8 @@
+package com.westssun.designpatterns._03_behavioral_patterns._19_observer._02_after;
+
+/**
+ * Observer
+ */
+public interface Subscriber {
+    void handleMessage(String message);
+}
