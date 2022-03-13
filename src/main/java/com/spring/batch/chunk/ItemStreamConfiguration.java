0d4f08981e31a56d1132d0a42a1d16cadae3914d
seohae <seohae0406@gmail.com>
2022-03-13 11:16:21 +0900

    (03.13) item stream

---
+++ src/main/java/com/spring/batch/chunk/ItemStreamConfiguration.java
@@ -0,0 +1,98 @@
+ package com.spring.batch.chunk;
+
+import com.spring.batch.chunk.custom.CustomItemProcessor;
+import com.spring.batch.chunk.custom.CustomItemReader;
+import com.spring.batch.chunk.custom.CustomItemWriter;
+import com.spring.batch.chunk.custom.Customer;
+import com.spring.batch.chunk.itemstream.CustomItemStreamReader;
+import com.spring.batch.chunk.itemstream.CustomItemStreamWriter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+--job.name=itemStreamJob
+ */
+@Configuration
+@RequiredArgsConstructor
+public class ItemStreamConfiguration {
+    // job 생성
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job itemStreamJob() {
+        return this.jobBuilderFactory.get("itemStreamJob")
+                /* step start */
+                .start(itemStreamStep1())
+                .next(itemStreamStep2())
+                .build();
+    }
+
+    @Bean
+    public Step itemStreamStep1() {
+        return stepBuilderFactory.get("itemStreamStep1")
+                // AbstractStep - open()
+                // TaskletStep - open()
+                // CompositeItemStream > open() (여기서 streams에 우리가 만든 Reader, Writer 이 담겨져있다)
+                // TaskletStep - update() (chunk 프로세스 진행 전 update)
+                // chunk 프로세스 수행
+                // chunk 프로세스 이후 update()
+
+                .<String, String>chunk(5)
+                .reader(customItemStreamReader())
+                .writer(customItemStreamWriter())
+                .build();
+    }
+
+    /**
+     * item stream writer
+     * @return
+     */
+    private ItemWriter<? super String> customItemStreamWriter() {
+        return new CustomItemStreamWriter();
+    }
+
+    /**
+     * item stream reader
+     * @return
+     */
+    private ItemReader<String> customItemStreamReader() {
+        List<String> items = new ArrayList<>(10);
+
+        for (int i = 0; i <= 10; i++) {
+            items.add(String.valueOf(i));
+        }
+
+        return new CustomItemStreamReader(items);
+    }
+
+    @Bean
+    public Step itemStreamStep2() {
+        return stepBuilderFactory.get("itemStreamStep2")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("chunkJobConfiguration step2 was executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+}
