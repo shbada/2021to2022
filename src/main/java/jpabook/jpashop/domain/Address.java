29f6d58fdc2afa3bf93e9eaec80aaee4e97673d0
seohae <seohae0406@gmail.com>
2022-03-17 21:46:19 +0900

    (03.17) entity 설정 마무리

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,10 +1,8 @@
 package jpabook.jpashop.domain;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.Embeddable;
-import javax.persistence.Entity;
 
 /* JPA 내장 파일 */
 @Embeddable // 임베디드 명시
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -22,21 +22,28 @@ public class Order {
      * Order와 Member는 N:1 관계
      * 연관관계 주인 정하기 : FK가 ORDERS에 MEMBER_ID 이기 때문에 FK가 있는 ORDERS가 주인테이블이다.
      */
+    // xxToOne 는 default 가 EAGER
     @ManyToOne(fetch = FetchType.LAZY) // 기본이 즉시로딩이므로, 지연로딩으로 설정하자.
+    // 아래 즉시로딩 문제 : N + 1개의 쿼리가 더 날라간다.
     // @ManyToOne(fetch = FetchType.EAGER) // 즉시로딩 : Order 조회시 Member 를 항상 조인하여 함께 가져온다. (무조건 지연로딩을 사용하자(LAZY))
     // @XToOne(OneToOne, ManyToOne) 관계는 기본이 즉시로딩이므로 지연로딩으로 설정하자.
     @JoinColumn(name = "member_id")
     private Member member;
 
+    // xxToMany 는 default 가 LAZY
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) /* 조회용이다. order 필드에 의해 매핑되었다. 연관관계 주인이 아니다. */
     // cascade
     // persist(orderItemA) persist(orderItemB) persist(orederItemC) persis(order) 이렇게 각각 호출을 해야하는데
     // cascade가 있으면 persist(order)만 있으면 orderItemA, orderItemB, orderItemC를 모두 저장해준다.
+
+    // 컬렉션은 필드에서 초기화하자. null 문제에서 안전하다. (초기화에 안전하다)
+    // 하이버네이터가 컬렉션을 감싸서 하이버네이트가 제공하는 내장 컬렉션으로 변경한다. (persist() 후에 ArrayList -> collection.internal.PersistenBag 이런식으로 바뀐다)
+    // 그래서 setxx()는 만들지말고, 이 컬렉션을 가급적이면 변경하지말자.
     private List<OrderItem> orderItems = new ArrayList<>();
 
     // FK 를 어디에 둬도 상관없다. 1:1
     // 접근성이 더 많은 곳에 놓자. 그렇다면 ORDER에 두겠다.
-    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL) // ORDER 를 저장할때 Delivery 도 저장해주겠다는 의미
     @JoinColumn(name = "delivery_id") /* 연관 관계의 주인 */
     private Delivery delivery;
 
