da49568b7a723bd2eb6d40097843083c1bd05b5c
seohae <seohae0406@gmail.com>
2022-03-24 19:49:07 +0900

    (03.24) 그리디 - 다른사람의 풀이 추가

--- src/com/algorithm/_01_그리디_구현/P42747_H_Index.java
+++ src/com/algorithm/_01_그리디_구현/P42747_H_Index.java
@@ -1,5 +1,7 @@
 package com.algorithm._01_그리디_구현;
 
+import java.util.Arrays;
+
 /**
  * @Date 2022/03/24
  * @URL https://programmers.co.kr/learn/courses/30/lessons/42747
@@ -9,6 +11,7 @@ public class P42747_H_Index {
 	    // write your code here
         P42747_H_Index main = new P42747_H_Index();
         System.out.println(main.solution(new int[]{10, 10, 10, 10, 10}));
+        System.out.println(main.solution2(new int[]{3, 0, 6, 1, 5}));
     }
 
     public int solution(int[] citations) {
@@ -37,5 +40,24 @@ public class P42747_H_Index {
         }
 
         return answer;
+    }
+
+    /**
+     * 다른 사람의 풀이
+     * @param citations
+     * @return
+     */
+    public int solution2(int[] citations) {
+        Arrays.sort(citations);
+        int result = 0;
+
+        for (int i = 0; i < citations.length; i++) {
+            // citations.length - i 보다 작은 값들 중에서 최소값
+            // 정렬 했으니까 작은 값들 중에서 결국 큰 값이랑 비교됨
+            int smaller = Math.min(citations[i], citations.length - i); // 5, 4, 3, 2, 1
+            result = Math.max(result, smaller);
+        }
+
+        return result;
     }
 }
