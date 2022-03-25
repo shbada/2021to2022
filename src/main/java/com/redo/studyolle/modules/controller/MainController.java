c7df7fbd2995b444f4b6b0532b410f15eb2c34a7
seohae <seohae0406@gmail.com>
2022-03-25 23:32:39 +0900

    (03.25) erd update

--- src/main/java/com/redo/studyolle/modules/controller/MainController.java
+++ src/main/java/com/redo/studyolle/modules/controller/MainController.java
@@ -12,9 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
     @GetMapping("/")
     public String home(@CurrentAccount Account account, Model model) {
-//        if (account != null) {
-//            model.addAttribute(account);
-//        }
+        if (account != null) {
+            model.addAttribute(account);
+        }
 
         return "index";
     }
