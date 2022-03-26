1097051b6c1776560525862b2609cd9942df3eaa
seohae <seohae0406@gmail.com>
2022-03-26 09:57:19 +0900

    (03.25) JsonConfiguration

---
+++ src/main/java/com/spring/batch/itemReader/json/JsonConfiguration.java
@@ -0,0 +1,65 @@
+package com.spring.batch.itemReader.json;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.json.JacksonJsonObjectReader;
+import org.springframework.batch.item.json.JsonItemReader;
+import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
+import org.springframework.batch.item.xml.StaxEventItemReader;
+import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.oxm.xstream.XStreamMarshaller;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Configuration
+public class JsonConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("batchJob")
+                .incrementer(new RunIdIncrementer())
+                .start(step1())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .<Customer, Customer>chunk(3)
+                .reader(customItemReader())
+                .writer(customItemWriter())
+                .build();
+    }
+
+    @Bean
+    public JsonItemReader<Customer> customItemReader(){
+        return new JsonItemReaderBuilder<Customer>()
+                .name("jsonItemReader")
+                // JsonItemReader > doRead() > JacksonJsonObjectReader > read() 호출
+                .jsonObjectReader(new JacksonJsonObjectReader<>(Customer.class))
+                .resource(new ClassPathResource("customer.json"))
+                .build();
+    }
+
+    @Bean
+    public ItemWriter<Customer> customItemWriter() {
+        return items -> {
+            for (Customer item : items) {
+                System.out.println(item.toString());
+            }
+        };
+    }
+}
