35fc94f1ed6db45e3959e5286ba1c10c354569d1
seohae <seohae0406@gmail.com>
2022-03-31 16:25:50 +0900

    (03.31) 알고리즘 문제풀이 +1

--- src/com/algorithm/_00_/Main.java
+++ src/com/algorithm/_00_/Main.java
@@ -1,19 +1,19 @@
 package com.algorithm._00_;
 
+import com.algorithm._05_BFS.A7576_토마토;
+
 /**
  * @Date 2022/03/25
  * @URL https://programmers.co.kr/learn/courses/30/lessons/42885
  */
 public class Main {
     public static void main(String[] args) {
-	    // write your code here
-        Main main = new Main();
-        System.out.println(main.solution());
+        // write your code here
+        A7576_토마토 main = new A7576_토마토();
+        main.solution();
     }
 
-    public int solution() {
-        int result = 0;
+    public void solution() {
 
-        return result;
     }
 }
--- src/com/algorithm/_07_Dynamic_DP/Main.java
+++ src/com/algorithm/_07_Dynamic_DP/Main.java
@@ -1,19 +1,19 @@
 package com.algorithm._07_Dynamic_DP;
 
+import com.algorithm._05_BFS.A7576_토마토;
+
 /**
  * @Date 2022/03/09
  * @URL https://
  */
 public class Main {
     public static void main(String[] args) {
-	    // write your code here
-        Main main = new Main();
-        System.out.println(main.solution());
+        // write your code here
+        A7576_토마토 main = new A7576_토마토();
+        main.solution();
     }
 
-    public int solution() {
-        int result = 0;
+    public void solution() {
 
-        return result;
     }
 }
