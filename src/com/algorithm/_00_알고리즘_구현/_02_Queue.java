fcebd06e2ed1b3e8d5ab142ba1dc7f3b3fd2b44a
westssum <seohae0406@gmail.com>
2022-04-03 00:50:53 +0900

    (04.03) 알고리즘 구현 move

---
+++ src/com/algorithm/_00_알고리즘_구현/_02_Queue.java
@@ -0,0 +1,24 @@
+package com.algorithm._00_알고리즘_구현;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Queue
+ */
+public class _02_Queue {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.offer(5); // push 5
+        queue.offer(3); // push 3
+        queue.offer(2); // push 2
+        queue.poll();  // pop 5
+        queue.offer(1); // push 1
+        queue.poll(); // pop 3
+
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
+    }
+}
