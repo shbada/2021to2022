b7fd560ee002249233c6f87367587a91515daf0d
westssum <seohae0406@gmail.com>
2022-04-07 17:04:50 +0900

    (04.07) logout

--- src/main/java/io/security/basicsecurity/config/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/config/SecurityConfig.java
@@ -8,10 +8,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
@@ -37,6 +41,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      */
     // UsernamePasswordAuthenticationFilter
 
+    /*
+       LogoutFilter > doFilter > 인증객체 꺼내옴 (UsernamePasswordAuthenticationToken)
+       logoutHandlers - 여러개의 핸들러를 가지고있음
+       - 이 중에 SecurityContextLogoutHandler 가 로그아웃 처리
+     */
+    // LogoutFilter
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -46,6 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // 그래서 아래에 formLogin() 쪽 로직에 permitAll()을 추가했다.
                 .anyRequest().authenticated();
 
+        /** login */
         http.formLogin()
                 // 인증을 해야한다면 여기로 이동한다.
                 //.loginPage("/loginPage") // springSecurity 가 기본적으로 제공하는 로그인 페이지 아닌 직접 만든 화면 설정
@@ -71,6 +83,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     }
                 })
                 .permitAll();
-        ; // Form Login 방식으로 진행하겠다 는 의미
+         // Form Login 방식으로 진행하겠다 는 의미
+
+        /** logout */
+        http
+                .logout()
+                .logoutUrl("/logout") // 기본적으로 post 방식이고 get 방식은 오류 발생 (따로 설정은 가능)
+                .logoutSuccessUrl("/login") // 로그아웃 성공 후 이동될 화면
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate(); // 세션 무효화
+                    }
+                })
+                // logoutSuccessUrl 와 비슷한 개념인데, url 설정은 이동할 페이지 설정이고 핸들러는 더 다양한 로직을 수행시킬 수 있다.
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.sendRedirect("/login");
+                    }
+                })
+                .deleteCookies("remember-me") // remember-me 인증시, remember-me 쿠키명으로 생성된 쿠키를 삭제함
+                ;
     }
 }
