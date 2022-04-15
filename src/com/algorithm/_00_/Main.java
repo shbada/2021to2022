a3498d9c031478d0f67e1d04095b5520357adf23
westssum <seohae0406@gmail.com>
2022-04-15 12:44:10 +0900

    (04.15) 알고리즘 문제풀이

--- src/com/algorithm/_00_/Main.java
+++ src/com/algorithm/_00_/Main.java
@@ -1,7 +1,5 @@
 package com.algorithm._00_;
 
-import com.algorithm._05_BFS.A7576_토마토;
-
 /**
  * @Date 2022/03/25
  * @URL https://programmers.co.kr/learn/courses/30/lessons/42885
@@ -9,7 +7,7 @@ import com.algorithm._05_BFS.A7576_토마토;
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        A7576_토마토 main = new A7576_토마토();
+        Main main = new Main();
         main.solution();
     }
 
