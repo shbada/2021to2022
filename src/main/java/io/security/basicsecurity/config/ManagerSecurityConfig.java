da68d71e92e324a3bff1918a934e4133dc5f1091
westssum <seohae0406@gmail.com>
2022-04-27 23:42:21 +0900

    (04.27) 인증 관리자 : AuthenticationManager

--- src/main/java/io/security/basicsecurity/config/ManagerSecurityConfig.java
+++ src/main/java/io/security/basicsecurity/config/ManagerSecurityConfig.java
@@ -1,35 +1,27 @@
 package io.security.basicsecurity.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.RequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity // WebSecurityConfiguration 등 여러 클래스들을 import 해준다.
-public class FormSecurityConfig extends WebSecurityConfigurerAdapter {
+public class ManagerSecurityConfig extends WebSecurityConfigurerAdapter {
 
     /*
      UsernamePasswordAuthenticationFilter
      > UsernamePasswordAuthenticationToken 생성
      > getAuthenticationManager().authenticate(authRequest);
        - manager 에게 인증 관리 요청
+       - 2개의 ProviderManager 을 생성함
+         * 1) parentAuthenticationManager 생성 (처음에 생성됨)
+         - 부모용으로 사용됨
+         * 2) ProviderManager 도 생성되는데, 이건 위 1) 부모 provider 을 가지는 default provider
        - ProviderManager.java > providers 를 리스트로 가지고있고, 이중 하나에 인증을 위임한다.
+         > 익명사용자의 경우 default 로 anonymousAuthenticationProvider 를 생성한다.
+       - parent 객체도 있다. (AuthenticationManager 타입)
+         > 만약 위 2)번 Manager 에서 Procider 을 찾지 못하면, 부모용 providerManager 을 한번더 탐색한다.
        - provider.authenticate(authentication); 호출
      > DaoAuthenticationProvider.java
        - getUserDetailsService().loadUserByUsername -> UserDetails 타입의 유저 객체 얻음
