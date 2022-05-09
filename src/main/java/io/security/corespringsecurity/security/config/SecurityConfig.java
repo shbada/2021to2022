6aff509e82ffddc038be60f10e888a7920271e45
westssum <seohae0406@gmail.com>
2022-05-09 22:05:04 +0900

    (05.09) 정적 자원 관리 - WebIgnore 설정

--- src/main/java/io/security/corespringsecurity/security/config/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package io.security.corespringsecurity.security.config;
 
 import antlr.BaseAST;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -49,8 +51,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         String password = passwordEncoder().encode("1111");
 
         auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
-        auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER");
-        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
+//        auth.inMemoryAuthentication().withUser("admin").password(password).roles("MANAGER");
+//        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
+        // 여러개의 ROLE을 줘야, ADMIN이 USER, MANAGER 이 접근 가능한 자원에 접근이 가능해진다.
+        auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER", "USER");
+        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN", "USER", "MANAGER");
+    }
+
+    /**
+     * 보안 필터를 적용할 필요가 없는 리소스를 설정해야한다. (.js, .css, .image 등)
+     * @param web
+     * @throws Exception
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // toStaticResources 에서 파일들에 대한 경로가 존재한다.
+        /*
+           .antMatchers("/").permitAll() 와 뭐가 다를까?
+           - 공통점 : 인증/권한 설정을 무조건 통과한다.
+           - .antMatchers("/").permitAll()는 보안 필터는 들어오고, 그 안에서 요청에 대해서 보안 통과가 되게끔 한다.
+             결국 보안필터는 거친다는 것이다.
+           - 그에비해 아래 ignoring 은 보안 필터 자체를 거치지 않는다.
+        */
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
     /**
