9291b9ca4545dc8fb44157d50c87eed325d45b27
kimseohae <seohae0406@gmail.com>
2022-06-24 20:18:12 +0900

    (06.24) commit test

--- src/main/java/io/security/corespringsecurity/controller/admin/AdminController.java
+++ src/main/java/io/security/corespringsecurity/controller/admin/AdminController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
-
     @GetMapping(value="/admin")
     public String home() throws Exception {
         return "admin/home";
