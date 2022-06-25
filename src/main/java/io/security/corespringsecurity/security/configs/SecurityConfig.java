f31b7234c6af6876c9f6438abe66263535950d8c
kimseohae <seohae0406@gmail.com>
2022-06-25 16:27:52 +0900

    (06.25) 3) 웹 기반 인가처리 DB 연동 - 주요 아키텍처 이해

--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -67,6 +67,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(final HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                /*
+                    Authentication(user), FilterInvocation(request(/user)), List<ConfigAttribute>("hasRole('USER')")
+                    위 3개 파라미터 정보가 필요하다.
+                    이때 권한 정보는 requestMap 에서 꺼내서 셋팅된다. 이는 DefaultFilterInvocationSecurityMetadataSource 여기에 있다.
+
+                    ExpressionBasedFilterInvocationSecurityMetadataSource.java
+
+                    DefaultFilterInvocationSecurityMetadataSource.java
+                    - private final Map<RequestMatcher, Collection<ConfigAttribute>> requestMap;
+
+                    디버깅을 해보면?
+                    1) FilterSecurityInterceptor.java
+                    - FilterInvocation 객체를 생성한다. (/user : 요청정보를 담는다)
+
+                    2) AbstractSecurityInterceptor.java (위 1)번의 부모클래스)
+                    - beforeInvocation() 메서드 수행 - this.obtainSecurityMetadataSource().getAttributes(object) 호출로 아래 로직 수행됨
+
+                    > security 초기화시, ExpressionBasedFilterInvocationSecurityMetadataSource.java 에서 권한 정보를 담은 map이 있음
+                    > DefaultFilterInvocationSecurityMetadataSource 클래스에게 요청하면 requestMap을 리턴해준다.
+                    > DefaultFilterInvocationSecurityMetadataSource 에서 getAttributes() 에서 요청 url에 맞는 권한을 리턴해주는 것이다.
+
+                    > 이제 요청정보/권한정보가 있으므로, 인증정보가 필요하다.
+
+                    3) AbstractSecurityInterceptor.java
+                    - authenticateIfRequired() 메서드 수행
+                    - 인증정보를 가져왔다.
+                    - 이제 accessDecisionManager.decide(authenticated, object, attributes); 를 수행한다. (모든 파라미터의 정보는 채워져있다.)
+
+
+                    리턴받은 requestMap에
+                 */
                 .antMatchers("/mypage").hasRole("USER") /* USER 권한 접속 가능 */
                 .antMatchers("/messages").hasRole("MANAGER") /* MANAGER 권한 접속 가능 */
                 .antMatchers("/config").hasRole("ADMIN") /* ADMIN 권한 접속 가능 */
