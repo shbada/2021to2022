606855f97bb08fc48e2657e4620441a1e0a63ad9
westssun <seohae0406@gmail.com>
2022-07-16 23:11:05 +0900

    (07.16) chapter02 synchronized

--- src/main/java/com/concurrency/chapter02/_02_LoggingWidget.java
+++ src/main/java/com/concurrency/chapter02/_02_LoggingWidget.java
@@ -15,6 +15,12 @@ public class _02_LoggingWidget extends _01_Widget {
      *
      * 암묵적인 락이 재진입 가능하지 않았다면, 이미 락을 누군가가 확보했기 때문에
      * super.doSomething()호출에서 락을 얻을 수 없게되고, 결과적으로 확보할 수 없는 락을 기다리면서 영원히 멈춰있었을 것이다.
+     *
+     * 1) 스레드 A, B 가 있다.
+     * 2) 스레드 A가 LoggingWidget 의 doSomething 메서드에 접근하여 락을 확보한다.
+     * 3) B 스레드가 LoggingWidget 의 락을 확보한다.
+     * 4) 스레드 A가 다시 기존 메서드로 돌아가기 위해 LoggingWidget 의 락을 확보하기 위해 대기한다.
+     * -> 그렇기에 병렬 프로그래밍에선 재진입성을 확보해야하고, 재진입이 가능하다는 전제하에 프로그래밍을 해야한다.
      */
     public synchronized void doSomething() {
         System.out.println(toString() + ": calling doSomething");
