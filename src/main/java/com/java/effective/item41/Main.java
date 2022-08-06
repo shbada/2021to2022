05ea2fcf0eea83185081eb79d1ca65cd06bd7031
westssun <seohae0406@gmail.com>
2022-08-06 21:26:42 +0900

    (08.06) item39 ~ item42

--- src/main/java/com/java/effective/item41/Main.java
+++ src/main/java/com/java/effective/item41/Main.java
@@ -2,7 +2,9 @@ package com.java.effective.item41;
 
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.Set;
 
 public class Main implements Serializable {
     // ObjectOutputStream
+//    Set
 }
