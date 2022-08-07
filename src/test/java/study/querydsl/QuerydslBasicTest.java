0ca219ab53f08b41164484f9692a6d92916bc1b6
westssun <seohae0406@gmail.com>
2022-08-07 11:09:25 +0900

    (08.07) 조인 - on절

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -356,4 +356,75 @@ public class QuerydslBasicTest {
                 .extracting("username")
                 .containsExactly("teamA", "teamB");
     }
+
+    /**
+     * 예) 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+     * JPQL: SELECT m, t FROM Member m LEFT JOIN m.team t on t.name = 'teamA'
+     * SQL: SELECT m.*, t.* FROM Member m LEFT JOIN Team t ON m.TEAM_ID=t.id and
+            t.name='teamA'
+     */
+    @Test
+    public void join_on_filtering() throws Exception {
+        /*
+            t=[Member(id=3, username=member1, age=10), Team(id=1, name=teamA)]
+            t=[Member(id=4, username=member2, age=20), Team(id=1, name=teamA)]
+            t=[Member(id=5, username=member3, age=30), null]
+            t=[Member(id=6, username=member4, age=40), null]
+         */
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team).on(team.name.eq("teamA"))
+                .fetch();
+//
+//        List<Tuple> result = queryFactory
+//                .select(member, team)
+//                .from(member)
+//                .join(member.team, team)
+//                .where(team.name.eq("teamA"))
+//                .fetch();
+        // on 절을 활용해 조인 대상을 필터링 할 때, 외부조인이 아니라 내부조인(inner join)을 사용하면,
+        // where 절에서 필터링 하는 것과 기능이 동일하다.
+        // 따라서 on 절을 활용한 조인 대상 필터링을 사용할 때, 내부조인 이면 익숙한 where 절로 해결하고,
+        // 정말 외부조인이 필요한 경우에만 이 기능을 사용하자.
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    /**
+     * 2. 연관관계 없는 엔티티 외부 조인
+     * 예) 회원의 이름과 팀의 이름이 같은 대상 외부 조인
+     * JPQL: SELECT m, t FROM Member m LEFT JOIN Team t on m.username = t.name
+     * SQL: SELECT m.*, t.* FROM Member m LEFT JOIN Team t ON m.username = t.name
+     *
+     * 하이버네이트 5.1부터 on 을 사용해서 서로 관계가 없는 필드로 외부 조인하는 기능이 추가
+     */
+    @Test
+    public void join_on_no_relation() throws Exception {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+
+        /*
+            t=[Member(id=3, username=member1, age=10), null]
+            t=[Member(id=4, username=member2, age=20), null]
+            t=[Member(id=5, username=member3, age=30), null]
+            t=[Member(id=6, username=member4, age=40), null]
+            t=[Member(id=7, username=teamA, age=0), Team(id=1, name=teamA)]
+            t=[Member(id=8, username=teamB, age=0), Team(id=2, name=teamB)]
+         */
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team).on(member.username.eq(team.name))
+                .fetch();
+
+//        일반조인: leftJoin(member.team, team) -> ID를 매칭한다.
+//        on조인: from(member).leftJoin(team).on(xxx)
+
+        for (Tuple tuple : result) {
+            System.out.println("t=" + tuple);
+        }
+    }
 }
