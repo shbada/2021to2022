f6d0937f0691c38aefd5e75b7112ad464400c980
westssun <seohae0406@gmail.com>
2022-08-14 23:14:49 +0900

    (08.14) 아이템 79. 과도한 동기화는 피하라

--- src/main/java/com/java/effective/item79/ObservableSet.java
+++ src/main/java/com/java/effective/item79/ObservableSet.java
@@ -44,6 +44,21 @@ public class ObservableSet<E> extends ForwardingSet<E> {
         }
     }
 
+    /**
+     * 외계인 메서드를 동기화 블록 바깥으로 옮긴다.
+     * @param element
+     */
+    private void notifyElementAdded2(E element) {
+        List<SetObserver<E>> snapshot = null;
+
+        synchronized (observers) {
+            snapshot = new ArrayList<>(observers);
+        }
+
+        for (SetObserver<E> observer : snapshot)
+            observer.added(this, element);
+    }
+
     @Override
     public boolean add(E element) {
         boolean added = super.add(element);
@@ -59,4 +74,18 @@ public class ObservableSet<E> extends ForwardingSet<E> {
             result |= add(element); // notifyElementAdded 호출
         return result;
     }
+
+//    private final List<SetObserver<E>> observers = new CopyOnWriteArrayList<>();
+//
+//    public void addObserver(SetObserver<E> observer) {
+//        observers.add(observer);
+//    }
+//
+//    public boolean removeObserver(SetObserver<E> observer) {
+//        return observers.remove(observer);
+//    }
+//
+//    private void notifyElementAdded(E element) {
+//        for (SetObserver<E> observer : observers)
+//            observer.added(this, element);
 }
