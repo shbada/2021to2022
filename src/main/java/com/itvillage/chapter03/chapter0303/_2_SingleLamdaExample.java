c68e6efc8646e9ad557ef402d9ead4af1c72fe6d
westssun <seohae0406@gmail.com>
2022-08-23 18:00:47 +0900

    (08.23) Single, Maybe, Completable

---
+++ src/main/java/com/itvillage/chapter03/chapter0303/_2_SingleLamdaExample.java
@@ -0,0 +1,17 @@
+package com.itvillage.chapter03.chapter0303;
+
+import com.itvillage.utils.DateUtil;
+import com.itvillage.utils.LogType;
+import com.itvillage.utils.Logger;
+import io.reactivex.Single;
+
+public class _2_SingleLamdaExample {
+    public static void main(String[] args){
+        Single<String> single = Single.create(emitter -> emitter.onSuccess(DateUtil.getNowDate()));
+
+        single.subscribe(
+                data -> Logger.log(LogType.ON_SUCCESS, "# 날짜시각: " + data),
+                error -> Logger.log(LogType.ON_ERROR, error)
+        );
+    }
+}
---
+++ src/main/java/com/itvillage/chapter03/chapter0303/_5_MaybeLamdaExample.java
@@ -0,0 +1,21 @@
+package com.itvillage.chapter03.chapter0303;
+
+import com.itvillage.utils.DateUtil;
+import com.itvillage.utils.LogType;
+import com.itvillage.utils.Logger;
+import io.reactivex.Maybe;
+
+public class _5_MaybeLamdaExample {
+    public static void main(String[] args){
+        Maybe<String> maybe = Maybe.create(emitter -> {
+            emitter.onSuccess(DateUtil.getNowDate());
+//            emitter.onComplete();
+        });
+
+        maybe.subscribe(
+                data -> Logger.log(LogType.ON_SUCCESS, "# 현재 날짜시각: " + data),
+                error -> Logger.log(LogType.ON_ERROR, error),
+                () -> Logger.log(LogType.ON_COMPLETE) // omComplete
+        );
+    }
+}
