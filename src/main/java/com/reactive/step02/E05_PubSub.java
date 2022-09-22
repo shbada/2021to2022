185f0d6b5ddfbeee20ba651cff84a09189aa23a2
westssun <seohae0406@gmail.com>
2022-09-22 22:02:16 +0900

    (09.22) step02 진행중

--- src/main/java/com/reactive/step02/E05_PubSub.java
+++ src/main/java/com/reactive/step02/E05_PubSub.java
@@ -6,16 +6,107 @@ import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
+import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * Publisher -> [Data1] -> Opreator -> [Data2(Data1 변환)] -> Opration2 -> [Data3(Data2 변환)] -> Subscriber
+ * 1) map (D1 -> f -> D2)
+ */
 @Slf4j
 public class E05_PubSub {
     public static void main(String[] args) {
-        Publisher<Integer> pub = new Publisher<Integer>() {
-            Stream<Integer> stream = Stream.iterate(1, a -> a + 1).limit(10);
-            Iterable<Integer> iter = stream.collect(Collectors.toList());
+        // pub -> data1 -> mapPub -> data2 -> logSub (위에서 아래로 데이터가 흐른다 : 다운스트림)
+        //              <- subscribe(logSub) (아래에서 위로 데이터가 흐른다 : 업스트림)
+        //              -> onSubscribe(s)
+        //              -> onNext
+        //              -> onNext
+        //              -> onComplete
+        Publisher<Integer> pub = iterPub(Stream.iterate(1, a -> a + 1).limit(10).collect(Collectors.toList()));
 
+        // 2단계 적용
+        Publisher<Integer> mapPub = mapPub(pub, (Function<Integer, Integer>) s -> s * 10);
+        Publisher<Integer> map2Pub = mapPub(pub, (Function<Integer, Integer>) s -> -s);
+
+        // 구독자
+        Subscriber<Integer> sub = logSub();
+
+        // 구독 시작
+//        pub.subscribe(sub);
+//        mapPub.subscribe(sub);
+        map2Pub.subscribe(sub);
+    }
+
+    /**
+     * 중간 연결
+     * @param pub
+     * @param integerIntegerFunction
+     * @return
+     */
+    private static Publisher<Integer> mapPub(Publisher<Integer> pub, Function<Integer, Integer> f) {
+        return new Publisher<Integer>() {
+            // pub -> data1 -> mapPub(Subscriber 생성) -> data2 -> logSub
+            @Override
+            public void subscribe(Subscriber<? super Integer> sub) { // logSub
+//                pub.subscribe(sub);
+                pub.subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        // 반드시 전달, 중간에서 할일은 없다
+                        sub.onSubscribe(s);
+                    }
+
+                    @Override
+                    public void onNext(Integer i) {
+//                        sub.onNext(i);
+                        sub.onNext(f.apply(i)); // function 적용
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        sub.onError(t);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        sub.onComplete();
+                    }
+                }); // 새로운 Subscriber
+            }
+        };
+    }
+
+    private static Subscriber<Integer> logSub() {
+        Subscriber<Integer> sub = new Subscriber<Integer>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                // Subscription 의 request 를 요청해야한다.
+                log.debug("onSubscribe");
+                s.request(Long.MAX_VALUE);
+            }
+
+            @Override
+            public void onNext(Integer i) {
+                log.debug("onNext:{}", i);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                log.debug("onError:{}", t);
+            }
+
+            @Override
+            public void onComplete() {
+                log.debug("onComplete");
+            }
+        };
+        return sub;
+    }
+
+    private static Publisher<Integer> iterPub(List<Integer> iter) {
+        Publisher<Integer> pub = new Publisher<Integer>() {
             // Publisher 의 구현해야하는 메서드
             @Override
             public void subscribe(Subscriber<? super Integer> sub) { // 호출하면 그때부터 데이터를 통지
@@ -45,33 +136,6 @@ public class E05_PubSub {
                 });
             }
         };
-
-        // 구독자
-        Subscriber<Integer> sub = new Subscriber<Integer>() {
-            @Override
-            public void onSubscribe(Subscription s) {
-                // Subscription 의 request 를 요청해야한다.
-                log.debug("onSubscribe");
-                s.request(Long.MAX_VALUE);
-            }
-
-            @Override
-            public void onNext(Integer i) {
-                log.debug("onNext:{}", i);
-            }
-
-            @Override
-            public void onError(Throwable t) {
-                log.debug("onError:{}", t);
-            }
-
-            @Override
-            public void onComplete() {
-                log.debug("onComplete");
-            }
-        };
-
-        // 구독 시작
-        pub.subscribe(sub);
+        return pub;
     }
 }
