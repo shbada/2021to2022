ec144da021db7f7e91d527af97e74729a9611b7a
westssun <seohae0406@gmail.com>
2022-09-25 12:07:50 +0900

    (09.25) step04 완료

--- src/main/java/com/reactive/ReactiveApplication.java
+++ src/main/java/com/reactive/ReactiveApplication.java
@@ -1,15 +1,47 @@
 package com.reactive;
 
+import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.Future;
+
 @SpringBootApplication
+@Slf4j
+@EnableAsync // @Async 비동기 수행을 위한 어노테이션 선언
 public class ReactiveApplication {
+    @Component
+    public static class MyService {
+        // 리스너 처리를 하고싶다면? ListenableFuture
+        @Async // 비동기
+        public ListenableFuture<String> hello() throws InterruptedException {
+            log.info("hello()");
+            Thread.sleep(2000);
+            return new AsyncResult<>("Hello"); // in Future
+        }
+
+//        public String hello() throws InterruptedException {
+//            log.info("hello()");
+//            Thread.sleep(1000);
+//            return "Hello";
+//        }
+    }
+
     @RestController
     public static class Controller {
         @RequestMapping("/hello")
@@ -37,8 +69,55 @@ public class ReactiveApplication {
         }
     }
 
+    /**
+     * bean으로 등록되어있으면 default 로 사용된다.
+     * 아무것도 등록되지 않으면 Simple** 로 default 로 사용된다.
+     * 여러개라면 @Async(value = "tp") value 에 빈 이름을 줄 수 있다.
+     * @return
+     */
+    @Bean
+    ThreadPoolTaskExecutor tp() { // ExecutorService 타입도 가능
+        ThreadPoolTaskExecutor te = new ThreadPoolTaskExecutor();
+        // 스레드풀을 정한 개수만큼 만들어놓는다. 첫번째 요청이 오면 개수만큼 만든다.
+        te.setCorePoolSize(10);
+        // 풀이 꽉찼는데 요청이 오면 100개 까지 만든다 -> 이렇게 이해하면 틀리다.
+        // corePoolSize 가 꽉차면 queueCapacity가 차고, queueCapacity가 꽉차면 maxPoolSize로 풀을 더 내준다.
+        te.setMaxPoolSize(100);
+        // 대기 큐를 몇개까지 가능할 것인가
+        te.setQueueCapacity(50);
+        te.setThreadNamePrefix("mythread"); // 스레드 이름 prefix 설정
+        te.initialize();
+
+        return te;
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(ReactiveApplication.class, args);
+        try (ConfigurableApplicationContext c = SpringApplication.run(ReactiveApplication.class, args)) {
+
+        }
+//        SpringApplication.run(ReactiveApplication.class, args);
+    }
+
+    @Autowired
+    MyService myService;
+
+    @Bean
+    ApplicationRunner run() {
+        return args -> {
+            log.info("run()");
+//            String res = myService.hello();
+//            log.info("exit: {}", res);
+
+            // 비동기
+//            Future<String> res = myService.hello();
+            ListenableFuture<String> res = myService.hello();
+
+            // callback
+            res.addCallback(s -> System.out.println(s), e -> System.out.println(e.getMessage()));
+
+            log.info("exit: {}", res.isDone());
+            log.info("result: {}", res.get());
+        };
     }
 
 }
