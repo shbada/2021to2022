caf7023218d704e7d2402327fe0407159e455ccd
westssun <seohae0406@gmail.com>
2022-09-28 23:38:00 +0900

    (09.28) generic 1강 진행중

---
+++ toby-generic/src/main/java/com/generic/TobyGenericApplication.java
@@ -0,0 +1,13 @@
+package com.generic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobyGenericApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobyGenericApplication.class, args);
+    }
+
+}
