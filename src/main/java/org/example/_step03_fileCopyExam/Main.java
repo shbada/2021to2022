387f073a390b426d914335bf3e00bee33e129c95
seohaem <seohae0406@gmail.com>
2022-11-03 23:09:39 +0900

    (11.03) ExecutorService

--- src/main/java/org/example/_step03_fileCopyExam/Main.java
+++ src/main/java/org/example/_step03_fileCopyExam/Main.java
@@ -3,6 +3,8 @@ package org.example._step03_fileCopyExam;
 import org.example._step03_fileCopyExam.utils.IOUtils;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 class CopyTask implements Runnable {
     String sourceFile;
@@ -32,6 +34,16 @@ public class Main {
         String destFile1 = "c.txt";
         String destFile2 = "d.txt";
 
+        // 100개 존재하면, 100개의 스레드?
+        // 10개의 스레드를 사용하여 이 100개의 파일 복사 작업을 수행하는게 좋겠다.
+        // -> ExecutorService
+
+        // 스레드 풀 크기가 5인 스레드 풀 실행자 생성
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        executor.execute(new CopyTask(sourceFile1, destFile1));
+        executor.execute(new CopyTask(sourceFile2, destFile2));
+
+        // 모든 태스크를 병렬로 수행
         CopyTask copyTask1 = new CopyTask(sourceFile1, destFile1);
         CopyTask copyTask2 = new CopyTask(sourceFile2, destFile2);
 
