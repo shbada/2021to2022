e13360a98369c9117879664383c6a60b7312b7fa
seohaem <seohae0406@gmail.com>
2022-11-05 10:43:18 +0900

    (11.05) docker-compose add

--- src/test/kotlin/com/studyolle/StudyolleKotlinApplicationTests.kt
+++ src/test/kotlin/com/studyolle/StudyolleKotlinApplicationTests.kt
@@ -1,13 +1,8 @@
 package com.studyolle
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class StudyolleKotlinApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
 
 }
