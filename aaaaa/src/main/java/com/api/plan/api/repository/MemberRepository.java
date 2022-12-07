a2f4cc2f5ca49222ab4e6c55a2a11c6522324a61
seohae <87924260+seohaem@users.noreply.github.com>
2022-12-07 14:42:19 +0900

    Delete aaaaa directory

--- aaaaa/src/main/java/com/api/plan/api/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.plan.api.repository;
-
-import com.api.plan.api.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-    Member findByMemberId(String memberId);
-}
